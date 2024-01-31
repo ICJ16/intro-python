2f45460d4c72f052e90a1ceda1bb9401ec516c10
ICJ16 <158080250+ICJ16@users.noreply.github.com>
2024-01-31 17:17:21 +0000

    completed daily assingment

--- kickoff warmup.py
+++ kickoff warmup.py
@@ -1,2 +1,2 @@
- "A computer program is a system that do what it is programmed to do"
- "lay the bread down put mustard and mayonise on it then add cheese lunch meat and lettuce"
+ # A computer program is a system that do what it is programmed to do "
+# lay the bread down put mustard and mayonise on it then add cheese lunch meat and lettuce 
